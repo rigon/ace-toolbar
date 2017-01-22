99868a599912cf1c46e971261dc1024be92f4154
rigon <ricar.goncalves@gmail.com>
2017-01-22 15:32:16 +0000

    Iterate over options

--- ace-toolbar.js
+++ ace-toolbar.js
@@ -27,6 +27,88 @@ function start(htmlObj, opts) {
     editor.getSession().setMode("ace/mode/" + opts.lang);
     editor.setShowPrintMargin(false);
     editor.focus();
+
+    for(var key in opts) {
+        switch(key) {
+            case "toolbar":
+            case "statusbar":
+            case "attrs":
+                // Options already processed
+                continue;
+            case "split":
+                // split: "none" "below" "beside",
+                break;
+            case "fontSize":
+                // fontSize: "12px"
+                break;
+            case "folding":
+                // folding: "manual" "markbegin" "markbeginend",
+                break;
+            case "keybinding":
+                // keybinding: "ace" "vim" "emacs" "custom",
+                break;
+            case "softWrap":
+                // softWrap: "off" "40" "80" "free",
+                break;
+            case "selectFullLine":
+                // selectFullLine: false,
+                break;
+            case "highlightActiveLine":
+                // highlightActiveLine: true,
+                break;
+            case "showHidden":
+                // showHidden: true,
+                break;
+            case "showIndentGuides":
+                // showIndentGuides: true,
+                break;
+            case "showHScroll":
+                // showHScroll: false,
+                break;
+            case "showVScroll":
+                // showVScroll: false,
+                break;
+            case "animateScroll":
+                // animateScroll: false,
+                break;
+            case "showGutter":
+                // showGutter: true,
+                break;
+            case "showPrintMargin":
+                // showPrintMargin: true,
+                break;
+            case "softTab":
+                // softTab: false,
+                break;
+            case "highlightSelectedWord":
+                // highlightSelectedWord: false,
+                break;
+            case "enablBehaviours":
+                // enablBehaviours: true,
+                break;
+            case "fadeFoldWidgets":
+                // fadeFoldWidgets: true,
+                break;
+            case "elasticTabstops":
+                // elasticTabstops: false,
+                break;
+            case "incrementalSearch":
+                // incrementalSearch: false,
+                break;
+            case "highlightToken":
+                // highlightToken: false,
+                break;
+            case "readOnly":
+                // readOnly: true,
+                break;
+            case "scrollPastEnd":
+                // scrollPastEnd: false,
+                break;
+            default:
+        }
+    }
+
+
     
     // function textEditorInsert(text) {
     //     markdownEditor.insert(text);
