1564eeed5e8e07f2c94176878fc3a53706080a18
rigon <ricar.goncalves@gmail.com>
2017-02-01 01:22:17 +0000

    Toolbar for markdown

--- mode/markdown.js
+++ mode/markdown.js
@@ -1,75 +1,63 @@
 
 function toolbar(editor, options) {
 
-    options.buttons
+    function bold() { editor.insert("****"); editor.selection.moveCursorLeft(); editor.selection.moveCursorLeft(); }
+    function italic() { editor.insert("**"); editor.selection.moveCursorLeft() }
+    function quote() { editor.insert("\n\n> "); }
+    function code() { editor.insert("\n\n    "); }
+    function ul() { editor.insert("\n\n - "); }
+    function ol() { editor.insert("\n\n 1. "); }
+    function header() { editor.insert("\n\n# "); }
+    function undo() { editor.getSession().getUndoManager().undo(); }
+    function redo() { editor.getSession().getUndoManager().redo(); }
 
-
-    function textEditorInsert(text) {
-        markdownEditor.insert(text);
-        markdownEditor.focus();
+    function image() { 
+        bootpopup({
+            title: "Add image",
+            content: [
+                { p: {text: "Insert image info here:"}},
+                { text: {label: "Title", name: "title", placeholder: "Description for image"}},
+                { url: {label: "Link", name: "link", placeholder: "Hyperlink for image"}}],
+            buttons: ["cancel", "ok"],
+            ok: function(data,e) {
+                editor.insert("![" + data[0].value + "](" + data[1].value + ")");
+            },
+        });
+    }
+    
+    function link() { 
+        bootpopup({
+            title: "Add hyperlink",
+            content: [
+                { p: {text: "Insert hyperlink info here:"}},
+                { text: {label: "Title", name: "title", placeholder: "Description for hyperlink"}},
+                { url: {label: "URL", name: "url", placeholder: "Hyperlink"}}],
+            buttons: ["cancel", "ok"],
+            ok: function(data,e) {
+                editor.insert("[" + data[0].value + "](" + data[1].value + ")");
+            },
+        });
     }
 
+    return {
+        buttons: {
+            boldtest: '<button type="button" class="btn btn-default btn-bold" title="Bold"><span class="fa fa-bold"></span></button>',
+            
+            bold: { title: "Bold", icon: "fa fa-bold", onclick: bold },
+            italic: { title: "Italic", icon: "fa fa-italic", onclick: italic },
 
+            quote: { title: "Quote text", icon: "fa fa-quote-left", onclick: quote },
+            code: { title: "Block of code", icon: "fa fa-code", onclick: code },
+            image: { title: "Add image", icon: "fa fa-image", onclick: image },
+            link: { title: "Hyperlink", icon: "fa fa-link", onclick: link },
+            
+            ol: { title: "Ordered list", icon: "fa fa-ol", onclick: ol },
+            ul: { title: "Unordered list", icon: "fa fa-ul", onclick: ul },
+            header: { title: "Header", icon: "fa fa-header", onclick: header },
+            table: { title: "Table", icon: "fa fa-table", onclick: table },
+            
+            undo: { title: "Undo", icon: "fa fa-undo", onclick: undo },
+            redo: { title: "Redo", icon: "fa fa-repeat", onclick: redo }
+        }
+    };
 }
-
-
-
-$("#editor-text .btn-bold").click(function() {
-    textEditorInsert("****");
-    markdownEditor.selection.moveCursorLeft();
-    markdownEditor.selection.moveCursorLeft();
-});
-$("#editor-text .btn-italic").click(function() {
-    textEditorInsert("**");
-    markdownEditor.selection.moveCursorLeft();
-});
-$("#editor-text .btn-quote").click(function() { textEditorInsert("\n\n> "); });
-$("#editor-text .btn-code").click(function() { textEditorInsert("\n\n    "); });
-$("#editor-text .btn-image").click(function() { 
-    bootpopup({
-        title: "Add image",
-        content: [
-            { p: {text: "Insert image info here:"}},
-            { text: {label: "Title", name: "title", placeholder: "Description for image"}},
-            { url: {label: "Link", name: "link", placeholder: "Hyperlink for image"}}],
-        buttons: ["cancel", "ok"],
-        ok: function(data,e) {
-            textEditorInsert("![" + data[0].value + "](" + data[1].value + ")");
-        },
-    });
-});
-$("#editor-text .btn-hyperlink").click(function() { 
-    bootpopup({
-        title: "Add hyperlink",
-        content: [
-            { p: {text: "Insert hyperlink info here:"}},
-            { text: {label: "Title", name: "title", placeholder: "Description for hyperlink"}},
-            { url: {label: "URL", name: "url", placeholder: "Hyperlink"}}],
-        buttons: ["cancel", "ok"],
-        ok: function(data,e) {
-            textEditorInsert("[" + data[0].value + "](" + data[1].value + ")");
-        },
-    });
-});
-$("#editor-text .btn-newpage").click(function() { 
-    bootpopup({
-        title: "Add new page",
-        content: [
-            { p: {text: "Insert new page info here:"}},
-            { text: {label: "Name", name: "Name", placeholder: "Page title", id: "newpage-name", onkeyup: function(obj) {
-                var url = S($(obj).val());
-                $("#newpage-url").val(url.slugify().s);
-            }}},
-            { text: {label: "URL", name: "url", placeholder: "URL for page", id: "newpage-url"}}],
-        buttons: ["cancel", "ok"],
-        ok: function(data,e) {
-            textEditorInsert("[" + data[0].value + "](" + data[1].value + ")");
-        },
-    });
-});
-$("#editor-text .btn-ul").click(function() { textEditorInsert("\n\n - "); });
-$("#editor-text .btn-ol").click(function() { textEditorInsert("\n\n 1. "); });
-$("#editor-text .btn-header").click(function() { textEditorInsert("\n\n# "); });
-$("#editor-text .btn-undo").click(function() { markdownEditor.getSession().getUndoManager().undo(); markdownEditor.focus(); });
-$("#editor-text .btn-redo").click(function() { markdownEditor.getSession().getUndoManager().redo(); markdownEditor.focus(); });
-					
