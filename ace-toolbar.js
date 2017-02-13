f6209c0ecd8a6964698b6c0036862193a30b8944
rigon <ricar.goncalves@gmail.com>
2017-02-13 16:29:27 +0000

    Refactoring

--- ace-toolbar.js
+++ ace-toolbar.js
@@ -1,19 +1,19 @@
 
 
 function acetoolbar(htmlElement, customOptions) {
-    var refObj = this;                              // Reference for this object
-    var element = $(htmlElement);                   // jQuery object of the selected HTML element
-    var editorElement = $("<div></div>");           // Content for Ace Editor
-    var editor = ace.edit(editorElement.get(0));    // Ace Editor
-    var toolbar = $("<div></div>");                 // Content for toolbar
-    var statusbar = $("<div></div>");               // Content for statusbar
+    var refObj = this;                          // Reference for this object
+    var element = $(htmlElement);               // jQuery object of the selected HTML element
+    var editor = $("<div></div>");              // Content for Ace Editor
+    var toolbar = $("<div></div>");             // Content for toolbar
+    var statusbar = $("<div></div>");           // Content for statusbar
+    var aceEditor = ace.edit(editor.get(0));    // Ace Editor
     var options;
 
 
     this.loadLangConfig = function(lang) {
         $.getScript("mode/" + lang + ".js", function(script, textStatus, jqXHR) {
-            var optionsToolbar = refObj.toolbar(editor, customOptions);
-            var optionsStatusbar = refObj.statusbar(editor, customOptions);
+            var optionsToolbar = refObj.toolbar(aceEditor, customOptions);
+            var optionsStatusbar = refObj.statusbar(aceEditor, customOptions);
 
             refObj.processOptions({
                 toolbar: optionsToolbar,
@@ -77,108 +77,107 @@ function acetoolbar(htmlElement, customOptions) {
 
 
     this.loadLangConfig(customOptions.lang);
-}
 
+    this.create = function() {
+        // Append toolbar
+        toolbar.attrs(this.options.toolbar.attrs);
+        element.append(toolbar);
 
-function create(elem, options) {
-    var toolbar = opts.toolbar;
-    $("<div></div>", toolbar.attrs).appendTo(obj);
+        // Append editor
+        editor.attrs(this.options.attrs);
+        element.append(editor);
 
-    var editorElement = $("<div></div>", opts.attrs).get(0);
-    obj.append(editorElement);
+        // Append statusbar
+        statusbar.attrs(this.options.statusbar.attrs);
+        element.append(statusbar);
 
-    
-    var statusbar = opts.statusbar;
-    $("<div></div>", statusbar.attrs).appendTo(obj);
+        
+        // Configure Ace Editor
+        aceEditor.setTheme("ace/theme/" + this.options.theme);
+        aceEditor.getSession().setMode("ace/mode/" + this.options.lang);
+        aceEditor.focus();
 
-
-    var editor = ace.edit(editorElement);
-    editor.setTheme("ace/theme/" + opts.theme);
-    editor.getSession().setMode("ace/mode/" + opts.lang);
-    editor.setShowPrintMargin(false);
-    editor.focus();
-
-    for(var key in opts) {
-        switch(key) {
-            case "toolbar":
-            case "statusbar":
-            case "attrs":
-                // Options already processed
-                continue;
-            case "split":
-                // split: "none" "below" "beside",
-                break;
-            case "fontSize":
-                // fontSize: "12px"
-                break;
-            case "folding":
-                // folding: "manual" "markbegin" "markbeginend",
-                break;
-            case "keybinding":
-                // keybinding: "ace" "vim" "emacs" "custom",
-                break;
-            case "softWrap":
-                // softWrap: "off" "40" "80" "free",
-                break;
-            case "selectFullLine":
-                // selectFullLine: false,
-                break;
-            case "highlightActiveLine":
-                // highlightActiveLine: true,
-                break;
-            case "showHidden":
-                // showHidden: true,
-                break;
-            case "showIndentGuides":
-                // showIndentGuides: true,
-                break;
-            case "showHScroll":
-                // showHScroll: false,
-                break;
-            case "showVScroll":
-                // showVScroll: false,
-                break;
-            case "animateScroll":
-                // animateScroll: false,
-                break;
-            case "showGutter":
-                // showGutter: true,
-                break;
-            case "showPrintMargin":
-                // showPrintMargin: true,
-                break;
-            case "softTab":
-                // softTab: false,
-                break;
-            case "highlightSelectedWord":
-                // highlightSelectedWord: false,
-                break;
-            case "enablBehaviours":
-                // enablBehaviours: true,
-                break;
-            case "fadeFoldWidgets":
-                // fadeFoldWidgets: true,
-                break;
-            case "elasticTabstops":
-                // elasticTabstops: false,
-                break;
-            case "incrementalSearch":
-                // incrementalSearch: false,
-                break;
-            case "highlightToken":
-                // highlightToken: false,
-                break;
-            case "readOnly":
-                // readOnly: true,
-                break;
-            case "scrollPastEnd":
-                // scrollPastEnd: false,
-                break;
-            default:
+        for(var key in opts) {
+            switch(key) {
+                case "toolbar":
+                case "statusbar":
+                case "attrs":
+                    // Options already processed
+                    continue;
+                case "split":
+                    // split: "none" "below" "beside",
+                    break;
+                case "fontSize":
+                    // fontSize: "12px"
+                    break;
+                case "folding":
+                    // folding: "manual" "markbegin" "markbeginend",
+                    break;
+                case "keybinding":
+                    // keybinding: "ace" "vim" "emacs" "custom",
+                    break;
+                case "softWrap":
+                    // softWrap: "off" "40" "80" "free",
+                    break;
+                case "selectFullLine":
+                    // selectFullLine: false,
+                    break;
+                case "highlightActiveLine":
+                    // highlightActiveLine: true,
+                    break;
+                case "showHidden":
+                    // showHidden: true,
+                    break;
+                case "showIndentGuides":
+                    // showIndentGuides: true,
+                    break;
+                case "showHScroll":
+                    // showHScroll: false,
+                    break;
+                case "showVScroll":
+                    // showVScroll: false,
+                    break;
+                case "animateScroll":
+                    // animateScroll: false,
+                    break;
+                case "showGutter":
+                    // showGutter: true,
+                    break;
+                case "showPrintMargin":
+                    // showPrintMargin: true,
+                    break;
+                case "softTab":
+                    // softTab: false,
+                    break;
+                case "highlightSelectedWord":
+                    // highlightSelectedWord: false,
+                    break;
+                case "enablBehaviours":
+                    // enablBehaviours: true,
+                    break;
+                case "fadeFoldWidgets":
+                    // fadeFoldWidgets: true,
+                    break;
+                case "elasticTabstops":
+                    // elasticTabstops: false,
+                    break;
+                case "incrementalSearch":
+                    // incrementalSearch: false,
+                    break;
+                case "highlightToken":
+                    // highlightToken: false,
+                    break;
+                case "readOnly":
+                    // readOnly: true,
+                    break;
+                case "scrollPastEnd":
+                    // scrollPastEnd: false,
+                    break;
+                default:
+            }
         }
     }
 }
-
 
 jQuery.fn.extend({
     acetoolbar: function (options) {
